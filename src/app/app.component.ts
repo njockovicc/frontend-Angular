55aad6bd73d99f5e398010b90d4b3c042eb46acc
shommy <ericmisha@gmail.com>
2025-11-21 09:46:42 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'proba';
+  isLoggedIn$: Observable<boolean>;
+  canReadUsers$: Observable<boolean>;
+  canSearchMachines$: Observable<boolean>;
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.isLoggedIn$ = this.authService.authUser$.pipe(map((user) => !!user));
+    this.canReadUsers$ = this.authService.hasPermission('read_user');
+    this.canSearchMachines$ = this.authService.hasPermission('search_machine');
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
